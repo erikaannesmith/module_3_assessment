1446be4e50b95bdb1519648a095a6ea30c175f5a
Erika Smith <erikaannesmith@gmail.com>
2018-01-02 11:05:07 -0700

    Begin to build out request spec for items

---
+++ spec/requests/items_request_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe "Items request" do
+
+  before :each do
+    @items = create_list(:item, 3)
+  end
+
+  xit "Can return json for all items" do
+
+  end
+
+  xit "Can return json for one item" do
+
+  end
+
+  xit "Can create an item" do
+
+  end
+
+  xit "Can delete an item" do
+
+  end
+
+end
