9d3eab732a9af115f58d36d5c1e287ed2d954c7f
Erika Smith <erikaannesmith@gmail.com>
2018-01-02 09:37:15 -0700

    begin to build out service

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -1,7 +1,18 @@
 class BestBuyService
 
-  def all_by_zipcode(zip)
+  def initialize
+    @conn = Faraday.new(url: "") do |faraday|
+      faraday.adapter Faraday.default_adapter
+    end
+  end
 
+  def all_by_zipcode(zip)
+    get_json("")
+  end
+
+  def get_json(url)
+    response = connection.get(url)
+    JSON.parse(response.body, symbolize_names: true)
   end
 
 end
