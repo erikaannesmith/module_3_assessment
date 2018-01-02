827a44d936305e381606f8a2cd8cf88eb240deca
Erika Smith <erikaannesmith@gmail.com>
2018-01-02 09:46:40 -0700

    Determined API call with postman, begin to build out initialize method on store

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,5 +1,13 @@
 class Store
 
+  def initialize(attrs = {})
+    @name = attrs[:name]
+    @city = attrs[:city]
+    @distance = attrs[:distance]
+    @phone = attrs[:phone]
+    @store_type = attrs[:store_type]
+  end
+
   def self.all_by_zipcode(zip)
     BestBuyService.new.all_by_zipcode(zip).map do |raw_store|
       Store.new(raw_store)
--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -1,17 +1,17 @@
 class BestBuyService
 
   def initialize
-    @conn = Faraday.new(url: "") do |faraday|
+    @conn = Faraday.new(url: "https://api.bestbuy.com") do |faraday|
       faraday.adapter Faraday.default_adapter
     end
   end
 
   def all_by_zipcode(zip)
-    get_json("")
+    get_json("/v1/stores(area(80203,25))?format=json&show=storeId,storeType,name&apiKey=ru99w3xp6qu5848qvbgrznxq")
   end
 
   def get_json(url)
-    response = connection.get(url)
+    response = @conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
   end
 
